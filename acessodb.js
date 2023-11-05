899aecdc3b899f3fc86f7443c7b35959e1a2db45
Jordana Rosa <jordananabarbosa@hotmail.com>
2023-11-05 17:35:40 -0300

    -

---
+++ acessodb.js
@@ -0,0 +1,44 @@
+const produto = document.getElementById("#produtos");
+const xmlHttp = new XMLHttpRequest();
+
+xmlHttp.onreadystatechange = () => {
+  if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+    let dados = JSON.parse(xmlHttp.responseText);
+    if (produto != null && dados != null && dados.length > 0) {
+      renderList(dados);
+    }
+  }
+};
+xmlHttp.open("GET", "/d/Desktop/BazarNaTela/db.json", true);
+xmlHttp.send();
+
+const renderList = (listaP) => {
+  const div = document.createElement("div");
+  div.classList.add("row");
+  div.classList.add("row-md-cols-4");
+  div.classList.add("justify-content-center");
+  div.classList.add("align-items-center");
+
+  for (let item of listaP) {
+    const div2 = document.createElement("div");
+    div2.classList.add("col");
+    div2.classList.add("d-flex");
+    div2.classList.add("justify-content-center");
+    div2.classList.add("align-items-center");
+    div2.innerHTML = `
+        <div class="card">
+                                                            <img src="${item.imagem}" class="card-img-top" alt="Product 1">
+                                                            <div class="card-body">
+                                                                <h5 class="card-title">${item.titulo}</h5>
+                                                                <p class="card-text">${item.descricao}</p>
+                                                                <p class="card-price">${item.preco}</p>
+                                                                <a href="#" class="favorite">
+                                                                    <i class="far fa-heart"></i> Favoritar
+                                                                </a>
+                                                            </div>
+                                                        </div>
+        
+        
+        `;
+  }
+};
